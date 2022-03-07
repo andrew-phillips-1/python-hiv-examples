cc3d189339773d5ed1a44480f5b7ba16409bab8f
Anastasis Georgoulas <a.georgoulas@ucl.ac.uk>
2022-03-07 19:22:24 +0000

    Add booleans in simple example

--- week01/01_simplePython.py
+++ week01/01_simplePython.py
@@ -8,13 +8,22 @@ y = 1.0
 # Strings
 name = "Python"
 
+# Logical values ("boolean")
+is_valid = True
+should_stop = False
+
 # Variables can be updated
 x = 0.6
 
-# And we can do arithmetic
+# And we can do arithmetic...
 z = 2*x + y
 x_squared = x**2
 x_over_y = x/y
+
+# ...and comparisons
+x_greater_y = x > y  # similarly <, >=, <=
+x_equal_y = x == y
+x_different_y = x != y
 
 # We can get the minimum or maximum of a pair of values
 w = min(x, 1.0)
@@ -28,3 +37,4 @@ square_root = math.sqrt(x - 0.1)
 # We can display information using print()
 print("Value of z = ", z)
 print("Value of z cubed = ", z**3)
+print("Is x different to y? ", x != y)
